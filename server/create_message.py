4af4453ed03574a333b865e8b4e5f75e1f6520e8
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2023-04-28 01:27:00 +0700

    done server

--- server/create_message.py
+++ server/create_message.py
@@ -2,29 +2,32 @@ import translate
 import openai
 import os
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 def create_message(profile1, profile2, list_matching_labels, sender, history):
-    print("check list_matching_labels")
-    print(list_matching_labels)
-    labels_content = []
-    for i in list_matching_labels:
-        if isinstance(i, dict):
-            for key, value in i.items():
-                labels_content.append(value)
-        else:
-            labels_content.append(profile1[i])
-        
-    # Find common interests
-    # common_interests = list(set(profile1["interests"]).intersection(profile2["interests"]))
-    common_interests = labels_content
-    # Create a specific prompt to highlight common interests and provided attributes
-    prompt = f"Write a {', '.join(labels_content)} statement from Profile 1 to Profile 2 based on their common interests: {common_interests}. Consider the following message history:\n\n"
+    formatted_labels = []
 
+    for label in list_matching_labels:
+        if isinstance(label, str) and label in profile1 and label in profile2:
+            formatted_labels.append(f"{label}-{profile1[label]}-{profile2[label]}")
+
+    print("Formatted list_matching_labels:")
+    print(formatted_labels)
+    
+    prompt = f"Write a statement from Profile 1 to Profile 2 based on what they have in common: {formatted_labels}. Consider the following message history:\n\n"
+
+    print("check history")
+    print(history)
+    
+    
     for message in history:
         prompt += f"{message['sender']} said: {message['msg']}\n"
 
+    print("check prompt")
+    print(prompt)
+    # return ["1","2","3"]
+    
     # Call the OpenAI API to generate a response
     response = openai.Completion.create(
         engine="text-davinci-002",
@@ -36,14 +39,15 @@ def create_message(profile1, profile2, list_matching_labels, sender, history):
     )
 
     # Extract the generated sentences from the response
-    list_of_responses = response.choices
-
+    list_of_responses = []
+    for choice in response.choices:
+        list_of_responses.append(choice.text.strip())
     # Print all responses from the API
-    for response in list_of_responses:
-        print(response.text.strip())
-        message_translate = translate.translate(response.text.strip(), "en", "vi")
-        print(message_translate)
-        print("------------------------")
+    # for response in list_of_responses:
+    #     print(response.text.strip())
+    #     message_translate = translate.translate(response.text.strip(), "en", "vi")
+    #     print(message_translate)
+    #     print("------------------------")
 
     # Return all responses from the API
     return list_of_responses
--- server/server.py
+++ server/server.py
@@ -1,3 +1,50 @@
+# from flask import Flask, jsonify, request
+# import random
+# import process_data
+# import json
+
+# app = Flask(__name__)
+
+
+# def process_choice(profile1, profile2, sender,history):
+#     choices = process_data.get_choice(profile1, profile2, sender,history)
+#     return {"choices": choices}
+
+
+
+# @app.route("/", methods=["POST"])
+# def process_json():
+#     content = request.json
+#     response = {}
+    
+#     if "profile1" in content and "profile2" in content:
+#         response["profile1"] = content["profile1"]
+#         response["profile2"] = content["profile2"]
+#         if "sender" in content:
+#             if content["sender"] == "P1":
+#                 response["sender"] = "P2"
+#             elif content["sender"] == "P2":
+#                 response["sender"] = "P1"
+#             response["msg_attr"] = content["msg_attr"]
+        
+                
+#         # Check if content includes chat history
+#         if "history" in content and isinstance(content["history"], list):
+#             response["history"] = content["history"]
+#         else:
+#             response["history"] = []
+            
+#         response["choices"] = process_choice(content["profile1"],content["profile2"], response["sender"],response["history"])["choices"]
+  
+#     else:
+#         response = {"error": "invalid request"}
+
+#     return jsonify(response)
+
+# if __name__ == "__main__":
+#     app.run()
+
+
 from flask import Flask, jsonify, request
 import random
 import process_data
@@ -6,39 +53,45 @@ import json
 app = Flask(__name__)
 
 
-def process_choice(profile1, profile2, sender,history):
-    choices = process_data.get_choice(profile1, profile2, sender,history)
+def process_choice(profile1, profile2, sender, history):
+    choices = process_data.get_choice(profile1, profile2, sender, history)
     return {"choices": choices}
 
 
-
-@app.route("/", methods=["POST"])
+@app.route("/getchoice", methods=["POST"])
 def process_json():
+    if not request.is_json:
+        return jsonify({"error": "Missing JSON data in request"}), 400
+
     content = request.json
     response = {}
-    
+
     if "profile1" in content and "profile2" in content:
         response["profile1"] = content["profile1"]
         response["profile2"] = content["profile2"]
+
         if "sender" in content:
             if content["sender"] == "P1":
                 response["sender"] = "P2"
             elif content["sender"] == "P2":
                 response["sender"] = "P1"
             response["msg_attr"] = content["msg_attr"]
-        
-                
+
         # Check if content includes chat history
         if "history" in content and isinstance(content["history"], list):
             response["history"] = content["history"]
         else:
             response["history"] = []
-        
-        response["choices"] = process_choice(content["profile1"],content["profile2"], response["sender"],response["history"])["choices"]
+
+        response["choices"] = process_choice(
+            content["profile1"], content["profile2"], response["sender"], response["history"]
+        )["choices"]
+
     else:
-        response = {"error": "invalid request"}
+        return jsonify({"error": "Invalid request: Missing required fields"}), 400
 
     return jsonify(response)
+
 
 if __name__ == "__main__":
     app.run()
