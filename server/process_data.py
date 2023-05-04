6ea6db2c093404dbb4afa3b67652f507cd5a8ba8
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2023-05-04 17:32:19 +0700

    done

--- server/process_data.py
+++ server/process_data.py
@@ -1,5 +1,5 @@
 import json
-import create_message
+# import create_message
 
 def get_matching_labels(profile1, profile2):
     matching_tags = []
@@ -31,7 +31,15 @@ def get_choice(profile1, profile2, sender, history):
     list_matching_labels = get_matching_labels(profile1, profile2)
     # print all content of list_matching_labels
     # print_content_of_list_matching_labels(list_matching_labels, profile1, profile2)
-    choices = create_message.create_message(profile1, profile2, list_matching_labels, sender, history)
+    # choices = create_message.create_message(profile1, profile2, list_matching_labels, sender, history)
+    choices = ["Hi", "Hello", "How are you?"]
+    sender_receive = ''
+    if sender == 'P2':
+        sender_receive = 'P1'
+    else:
+        sender_receive = 'P2'
+        
+    sender = sender_receive
     return choices
 
 
