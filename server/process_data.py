f1a717ec2f2d6536516609a7e12e9ee3da2c1466
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2023-05-04 20:00:35 +0700

    nop bai thoi em oi

--- server/process_data.py
+++ server/process_data.py
@@ -1,5 +1,18 @@
 import json
-# import create_message
+import create_message
+
+
+
+def remove_said(string):
+    string = string.replace("P2 said:", "")
+    string = string.replace("P1 said:", "")
+    return string
+
+def replace_names(string,profile1, profile2):
+    string = string.replace("P1", profile1['Name'])
+    string = string.replace("P2", profile2['Name'])
+    return string
+
 
 def get_matching_labels(profile1, profile2):
     matching_tags = []
@@ -31,16 +44,21 @@ def get_choice(profile1, profile2, sender, history):
     list_matching_labels = get_matching_labels(profile1, profile2)
     # print all content of list_matching_labels
     # print_content_of_list_matching_labels(list_matching_labels, profile1, profile2)
-    # choices = create_message.create_message(profile1, profile2, list_matching_labels, sender, history)
-    choices = ["Hi", "Hello", "How are you?"]
+    choices = create_message.create_message(profile1, profile2, list_matching_labels, sender, history)
+    # choices = ["Hi", "Hello", "How are you?"]
     sender_receive = ''
     if sender == 'P2':
         sender_receive = 'P1'
     else:
         sender_receive = 'P2'
         
+    result = [];
+    
+    for choice in choices:
+        result.append(replace_names(remove_said(choice), profile1, profile2))    
+    
     sender = sender_receive
-    return choices
+    return result
 
 
 
